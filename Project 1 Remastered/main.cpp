b2ceb273c2857fda0708998bb3210c04b7d110dc
Randy-ZixiangZhang <randy-zixiang@Randy-ZixiangZhangs-MacBook-Pro.local>
2022-06-26 10:08:21 +0200

    remove redudant comments, nicer structure

--- Project 1 Remastered/main.cpp
+++ Project 1 Remastered/main.cpp
@@ -18,21 +18,6 @@ class Worker
 		bool m_terminate;
 		bool m_running;
 		
-//		static void work(Worker* p){
-//			//start the thread
-//			p->prepare();
-//			//a loop for step
-//			while (!p->m_terminate) 
-//				{
-//					//m_terminate = step();//step return false then while continues
-//					std::cout << "running loop" << std::endl;
-//					p->m_terminate = true;
-//				}
-//			
-//			std::cout << "Joinable? " << p->m_thread.joinable() << "yes 1 no 0"<< std::endl;
-//			//m_thread.join();//???????Core Dumped
-//		};
-//		
 		void work(){
 			//start the thread
 			prepare();
@@ -43,9 +28,7 @@ class Worker
 					std::cout << "running loop" << std::endl;
 					m_terminate = true;
 				}
-			
 			std::cout << "Joinable? " << m_thread.joinable() << "yes 1 no 0"<< std::endl;
-			//m_thread.join();//???????Core Dumped
 		};
 		
 		
@@ -55,12 +38,10 @@ class Worker
 			m_running = true;
 			std::cout << "started" << std::endl;
 			m_thread = std::thread(&Worker::work,this);
-//			m_thread = std::thread{work,this};
 
 		};
 		void stop(){
 			std::cout << "stop called" << std::endl;
-			//m_thread.join();
 			m_terminate = true; //set m_terminiate to true, try to influence the work loop;
 		};
 		Worker()
@@ -91,85 +72,11 @@ class Producer : public Worker
 	};
 };
 
-
-//{
-//	private:
-//		Type_buf& buffer;	
-//	
-//	public:
-//	
-//		Producer(Type_buf& buf):buffer(buf){
-//		Producer(){
-//			//buffer = buf;//Error when initialize in constructor
-//			std::cout << "ssss" << std::endl;
-//			};
-//			
-//			
-//	protected:
-//		void prepare() override {	
-//			std::cout << "sssdddds" << std::endl;
-//			abort();
-//		}
-//		bool step() override {
-//			std::cout << "ffffffffffffffffff" << std::endl;
-//			return true;continue the loop
-//		}
-//};
-
-
-
-//			Type_Element old_value = {0,0};
-//			Type_Element new_value = {0,0};
-//			std::mt19937 engine(time(0));
-//			std::uniform_int_distribution<> uniformDist(1,100);
-//			int j = 5;
-//			//buf.Push(old_value);
-//			
-//			std::cout << buffer.Empty()<<"asdasdsa" << std::endl;
-
-
-
-//template<typename Type_buf>
-//class Real_Producer : public Producer<Type_buf>
-//{
-//};
-
-//class Consumer : public Worker
-//{
-
-//};
-
-//class Real_Consumer : public Producer<Type_buf>
-//{
-//};
-
 struct Point
 {
 	float x;
 	float y;
 };
-
-//struct Parameter
-//{
-//	const float parameter1[6] = {0,0,0,0.16,0,0};
-//	const float parameter2[6] = {0.85,0.0, -0.04,0.85,0,1.6};
-//	const float parameter3[6] = {0.2,-0.26,0.23,0.22,0,1.6};
-//	const float parameter4[6] = {-0.15,0.28,0.26,0.24,0,0.44};
-
-//};
-
-
-//template<typename T>
-//class Producer(T xxx)
-//{
-//	private:
-//	Buffer<T>& m_buffer;
-//	
-//	public:
-//	virtual void step(){
-//		
-//	}
-//};
 
 #define THREAD_NUM 4
 #define POINT_NUM 5000000
@@ -193,7 +100,6 @@ struct circular_buf{
 		{
 			buffer_queue.pop();			
 		}
-
 	}
 	void Push(T element){
 
@@ -201,26 +107,21 @@ struct circular_buf{
 			buffer_queue.pop();
 		};
 		buffer_queue.push(element);
-
 	}
 	
 	int Size(){
 		return buffer_queue.size();
 	}
-	
 	T Front(){
 		return buffer_queue.front();
 	}
-	
 	T Back(){
 		return buffer_queue.back();
 	}
-	
 	bool Empty()
 	{
 		return buffer_queue.empty();
 	}
-	
 	T Takefirst(){
 		T first_element = buffer_queue.front();
 		buffer_queue.pop();
@@ -234,9 +135,6 @@ void Drawing(circular_buf<Point>& buf,cimg_library::CImg<unsigned char>& bg){
 	bool buf_empty;
 	
 	while(!doneflag){
-  		
-  		{
-		
 		muTex.lock();
 		//std::cout << "buffer size" <<buf.Size() << std::endl;
 		if (!buf.Empty()){
@@ -246,14 +144,9 @@ void Drawing(circular_buf<Point>& buf,cimg_library::CImg<unsigned char>& bg){
 			//bg.draw_circle((point_read.y*1000),(point_read.x*1000+2200),10,bluegreen);
 		}
 		muTex.unlock();
-		}
-		}
-			
 		
-	
-		
+		}
 };
-
 
 
 void CalculateNextPoint(circular_buf<Point>& buf) //Array are default passed by pointers
@@ -264,9 +157,6 @@ void CalculateNextPoint(circular_buf<Point>& buf) //Array are default passed by 
 	std::uniform_int_distribution<> uniformDist(1,100);
 	int j = 5;
 
-	//bg.draw_point(150,200,bluegreen);
-//	
-//	
 	for (int i =1; i < POINT_NUM; i++){
 		auto random1_100 = uniformDist(engine);
 		
@@ -289,8 +179,7 @@ void CalculateNextPoint(circular_buf<Point>& buf) //Array are default passed by 
 		{
 			j = 3;
 		};
-		//std::cout << uniformDist(engine) << std::endl;
-//		
+
 		
 		new_value.x = para[j][0]*old_value.x + para[j][1]*old_value.y + para[j][4];
 		new_value.y = para[j][2]*old_value.x + para[j][3]*old_value.y + para[j][5];
@@ -300,10 +189,7 @@ void CalculateNextPoint(circular_buf<Point>& buf) //Array are default passed by 
 	  		muTex.lock();
 			buf.Push(new_value);
 			muTex.unlock();
-  		}
-
-//		std::cout << "buf Size" << buf.Size() << std::endl ;
-		
+  		}		
 	}
 	doneflag = true;
 };
@@ -314,19 +200,6 @@ void CalculateNextPoint(circular_buf<Point>& buf) //Array are default passed by 
 
 int main(void)
 {
-
-
-
-//	Worker worker1;
-//	worker1.start();
-//	worker1.stop();
-	//Producer<circular_buf<Point>> producer1(*buf);
-//	Producer producer1;
-//	producer1.start();//without start, core dumped
-
-//	Producer producer1;
-//	producer1.start();
-
 
 	auto sta = std::chrono::steady_clock::now();
 	
@@ -339,12 +212,12 @@ int main(void)
 
 
 	std::thread t1;
-//	std::thread t1(CalculateNextPoint,std::ref(buf));
+
 	t1 = std::thread(CalculateNextPoint,std::ref(*buf));
 	std::thread t5(CalculateNextPoint,std::ref(*buf));
 	std::thread t6(CalculateNextPoint,std::ref(*buf));
-	
-	//std::thread t1([&](){CalculateNextPoint(Buffer,count,parameter)});
+
+
 	std::thread t2(Drawing,std::ref(*buf),std::ref(*bg));
 	std::thread t3(Drawing,std::ref(*buf),std::ref(*bg));
 	std::thread t4(Drawing,std::ref(*buf),std::ref(*bg));
@@ -356,9 +229,6 @@ int main(void)
 	t4.join();
 	t5.join();
 	t6.join();
-
-	
-
 	
 	std::chrono::duration<double> dur = 
 	std::chrono::steady_clock::now() - sta;
